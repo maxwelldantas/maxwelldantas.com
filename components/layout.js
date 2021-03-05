f2e0c3c7dcec29d0334944b405a8c9aa05aca1f8
Maxwell Dantas <mdszdev@gmail.com>
2021-03-04 21:07:56 -0300

    :rocket: Alteração do título do blog

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Maxwell Dantas'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Blog do Max'
 
 export default function Layout({ children, home }) {
     return (
