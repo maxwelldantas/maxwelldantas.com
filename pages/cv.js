2114c2670cce2c5e55c00720d330b227ef33934e
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-06-10 01:23:52 -0300

    Atualização altura

--- pages/cv.js
+++ pages/cv.js
@@ -3,27 +3,15 @@ import React, { useEffect, useState } from "react";
 export default function Cv() {
   const pdfUrl = "/files/Maxwell_CV.pdf";
   const [alturaIframe, setAlturaIframe] = useState(0);
-  const [larguraIframe, setLarguraIframe] = useState("100%");
 
   useEffect(() => {
     const alturaTela = window.innerHeight;
     const novaAlturaIframe = alturaTela - 115;
     setAlturaIframe(novaAlturaIframe);
-
-    const larguraDisponivel = window.innerWidth;
-    const novaLarguraIframe = larguraDisponivel;
-    setLarguraIframe(`${novaLarguraIframe}px`);
   }, []);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
+    <div>
       <h1
         style={{
           display: "flex",
@@ -35,14 +23,10 @@ export default function Cv() {
       </h1>
       <iframe
         src={pdfUrl}
-        width={larguraIframe}
+        width="100%"
         height={`${alturaIframe}px`}
         title="Curriculum Vitae"
-        style={{
-          border: "20px solid rgb(60, 60, 60)",
-          maxWidth: "100%",
-          boxSizing: "border-box",
-        }}
+        style={{ border: "20px solid rgb(60, 60, 60)" }}
       />
     </div>
   );
