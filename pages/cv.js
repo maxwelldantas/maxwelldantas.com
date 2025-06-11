4f2734e89b4de174ac4e2e5973e31d2109b617d6
Maxwell Dantas <maxwelldsouza@hotmail.com>
2025-06-10 23:28:22 -0300

    Atualização para exibir PDF pelo Google Drive

--- pages/cv.js
+++ pages/cv.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Cv() {
-  const pdfUrl = "/files/Maxwell_CV.pdf";
+  const pdfUrl =
+    "https://drive.google.com/file/d/1Q0i29C52gbcuHMB7r1MRUTePP15tCzWC/preview";
+
+  const iframeWidth = "50%";
+  const [iframeHeight, setIframeHeight] = useState(0);
+
+  useEffect(() => {
+    const calculateIframeDimensions = () => {
+      const alturaTela = window.innerHeight;
+      const novaAlturaIframe = alturaTela - 115;
+      setIframeHeight(novaAlturaIframe);
+    };
+
+    calculateIframeDimensions();
+
+    window.addEventListener("resize", calculateIframeDimensions);
+
+    return () => {
+      window.removeEventListener("resize", calculateIframeDimensions);
+    };
+  }, []);
 
   return (
     <div>
@@ -10,11 +30,23 @@ export default function Cv() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+          marginBottom: "20px",
         }}
       >
         Curriculum Vitae
       </h1>
-      <embed src={pdfUrl} type="application/pdf" width="100%" height="820px" />
+      <iframe
+        src={pdfUrl}
+        width={iframeWidth}
+        height={`${iframeHeight}px`}
+        allowfullscreen="false"
+        title="Curriculum Vitae"
+        style={{
+          border: "20px solid rgb(60, 60, 60)",
+          display: "block",
+          margin: "0 auto",
+        }}
+      />
     </div>
   );
 }
